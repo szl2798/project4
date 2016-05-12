90a81aa5b0e1f2c9e909e5827d8f5a5b7d5aba45
hassib291 <hassib291@hotmail.com>
2016-05-12 06:31:39 +0000

    a minor update

--- Transaction.java
+++ Transaction.java
@@ -1,5 +1,21 @@
 public class Transaction{
     private int bAmount;
     private int playerId;
-    private int bType;
+    private String bType;
+    private int chips1d , chips5d, chips25d, chips100d;
+    
+    public Transaction(int bitAmount, int player, int chips1, int chips5, int chips25, int chips100, String bitType){
+        bAmount = bitAmount;
+        playerId = player;
+        bType = bitType;
+        chips1d = chips1;
+        chips5d = chips5;
+        chips25d = chips25;
+        chips100d = chips100;
+    }
+    
+    public String toString(){
+        String transaction ="";
+        
+    }
 }
