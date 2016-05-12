47906aae6c8832183957af90af3e075a750e7c35
hassib291 <hassib291@hotmail.com>
2016-05-12 13:07:50 -0700

    edit the constructor

--- Transaction.java
+++ Transaction.java
@@ -3,8 +3,10 @@ public class Transaction{
     private int playerId;
     private String bType;
     private int chips1d , chips5d, chips25d, chips100d;
+    private int transactionId;
     
-    public Transaction(int bitAmount, int player, int chips1, int chips5, int chips25, int chips100, String bitType){
+    public Transaction(int id, int bitAmount, int player, int chips1, int chips5, int chips25, int chips100, String bitType){
+        transactionId = id;
         bAmount = bitAmount;
         playerId = player;
         bType = bitType;
@@ -16,6 +18,7 @@ public class Transaction{
     
     public String toString(){
         String transaction ="";
-        
+        transaction += transactionId + "\t" + playerId + "\t" + bAmount + "\t(\t" + chips100d + "\t" + chips25d +"\t"
+            + chips5d +"\t" + chips1d;
     }
 }
