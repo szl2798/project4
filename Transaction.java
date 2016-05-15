d3afb9dd9a57cd8a1a2b434229a19e1b20c7b394
hassib291 <hassib291@hotmail.com>
2016-05-15 14:41:52 -0700

    removed abstract method and the comment

--- Transaction.java
+++ Transaction.java
@@ -4,8 +4,7 @@ public class Transaction{
     private String bType;
     private int chips1d , chips5d, chips25d, chips100d;
     private int transactionId;
-    // comment
-    private int ff;
+
     public Transaction(int id, int bitAmount, int player, int chips1, int chips5, int chips25, int chips100, String bitType){
         transactionId = id;
         bAmount = bitAmount;
@@ -22,6 +21,4 @@ public class Transaction{
         transaction += transactionId + "\t" + playerId + "\t" + bAmount + "\t(\t" + chips100d + "\t" + chips25d +"\t"
             + chips5d +"\t" + chips1d;
     }
-    
-    public abstract void print();
 }
